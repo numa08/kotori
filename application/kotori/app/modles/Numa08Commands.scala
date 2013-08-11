e3125e086ec81db8e6491ddd432f9758848ef2e0
numa08 <n511287@gmail.com>
2013-08-11 20:23:30 +0900

    とりあえずの例外処理

--- application/kotori/app/modles/Numa08Commands.scala
+++ application/kotori/app/modles/Numa08Commands.scala
@@ -5,7 +5,11 @@ import scala.sys.process._
 class Numa08Commands extends Command {
 
   def doIt() {
-    "cd /usr/local/www/numa08/ && git pull origin master".run
+    try {
+      "cd /usr/local/www/numa08/ && git pull origin master".run
+    } catch {
+      case e:Exception => println("oh...")
+    }
   }
 
 }
